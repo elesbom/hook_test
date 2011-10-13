4633821b2eb1f2a3e433d8e596d1221e59ac7c1d
Rafael dos Santos Gonçalves <rafael.goncalves@corp.globo.com>
2011-10-13 18:32:08 -0300

    arquivo setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name = 'apresentador',
+    version = '1.2.7',
+    description = u'App do projeto Variedades que contem o modelo de Apresentador'.encode('utf-8'),
+    author = 'Entretenimento',
+    author_email = 'entretenimento@corp.globo.com',
+    url = 'http://ngit.globoi.com/base-variedades/apresentador',
+    packages = find_packages(),
+    zip_safe = False,
+    include_package_data = True,
+)
