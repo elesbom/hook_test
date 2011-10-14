fc9cf9693d547f1f13a7486b388e112465c3a8e6
Rafael dos Santos Gonçalves <rafael.goncalves@corp.globo.com>
2011-10-14 10:43:40 -0300

    Versao 1.2.9 criada

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'apresentador',
-    version = '1.2.8',
+    version = '1.2.9',
     description = u'App do projeto Variedades que contem o modelo de Apresentador'.encode('utf-8'),
     author = 'Entretenimento',
     author_email = 'entretenimento@corp.globo.com',
